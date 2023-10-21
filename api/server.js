cb19f7114656e43f7c3b0b0f5cac794c59bc4b29
jvshoffmann <jvshoffmann@gmail.com>
2023-10-21 16:48:37 -0300

    conexão com banco de dados funcionando

--- api/server.js
+++ api/server.js
@@ -1,15 +1,43 @@
-const express = require("express")
-const app = express()
+const cors = require('cors');
+const express = require("express");
+const bodyParser = require('body-parser');
+const client = require('./config/database');
+const bcrypt = require('bcrypt');
 
-const cors = require("cors")
+
+/*const cors = require("cors")
 app.use(cors())
 /*app.get("/", function(req, res) {
   res.send("It's working!")
 })*/
-app.get("/", function(req, res) {
+/*app.get("/", function(req, res) {
     res.send({"name": "Jane Doe"}) // Should be json format
   })
 
 app.listen(3000, () => {
   console.log("app listening on port 3000")
-})
+})*/
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const result = await client.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id', [username, email, hashedPassword]);
+    res.json({ success: true, userId: result.rows[0].id });
+  } catch (error) {
+    
+    res.status(500).json({ success: false, message: 'Erro ao registrar.' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
